5442a06963c77350ad8355e0765a43afbf9c8dca
René Gieling <github@dartcafe.de>
2022-02-15 22:47:10 +0100

    move steps to single functions and make the script callable from anywhere (#1)

--- README.md
+++ README.md
@@ -6,24 +6,27 @@ This script
 - makes sure, missing indices and columns are added
 - updates apps, if available
 - updates the nextcloud instance
-- supports multiple instances in one account
+- supports multiple instances inside one hoster account
 
 Make sure, that this script runs on your installation. It works for me. Use it on your own risk!
 Before the first run, edit your installations.txt as described below and in the updater script.
 To update call the update.sh file
 
+# Important changes
+* moved the installations.txt to the script directory
+
 # Installation
 ## Via git (recommended)
 * log in to your server via ssh
-* create a new directory inyour account i.e. /helpers
+* create a new directory in your account i.e. /helpers
 * call `git clone https://github.com/dartcafe/all-inkl-nc-updater.git`
-* cd into the new created directory (`cd helpers` in this example)
+* cd into the new created directory (`cd helpers/all-inkl-nc-updater` in this example)
+* call `chmod 744 ./all-inkl-nc-updater/update.sh`
 * call `touch installations.txt`
 * open installations.txt in your editor and enter your installations relative to your accounts root
   * i.e. if your install directory (nextcloud root) is /www/htdocs/w000000/domain.com/nextcloud
-  * then add "domain.com/nextcloud" to the installations.txt
-  * for multiple installations add more lines with the correct path to the installation
-* call `chmod 744 ./all-inkl-nc-updater/update.sh`
+  * then add "domain.com/nextcloud" to the `installations.txt`
+  * for multiple installations add more lines with the correct path to the installation (see `installations.txt.sample`)
 
 ## get updates (via git)
 * log in to your server via ssh
--- update.sh
+++ update.sh
@@ -19,7 +19,7 @@
 # - You should have received a copy of the GNU Affero General Public License
 # - along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# Autoupdater 0.1 for nextcloud installation of all-inkl.com customers
+# Autoupdater 0.2 for nextcloud installation of all-inkl.com customers
 # This script
 # - sets the memory_limit in .user.ini
 # - makes sure, missing indices and columns are added
@@ -28,87 +28,133 @@
 # - supports multiple instances in one account
 #
 # Make sure, that this script runs on your installation. It works for me.
-# Use it on your own risk
+# Use it at your own risk
 
-# define the php_limit
-php_memory_limit="512M"
+function test_installations() {
+    if [[ ! -f ${installations} ]]; then
+        logger "- installations.txt missing"
+        exit 1
+    fi
+}
+
+function logger()
+{
+    local text="$1"
+    if [[ "${text}" != "" ]] ; then
+			echo -e "\e[33m${text}\e[0m"
+    fi
+}
+
+function occ_add_indices()
+{
+	# occ db:add-missing-indices and :add-missing-columns is called blind
+	logger "- run occ db:add-missing-indices"
+	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-indices
+	logger "- run occ db:add-missing-columns"
+	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-columns
+	logger "- run occ db:add-missing-primary-keys"
+	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-primary-keys
+	logger "- run occ -n db:convert-filecache-bigint"
+	php -d memory_limit=$php_memory_limit $nc_base/occ -n db:convert-filecache-bigint
+}
+
+function set_php_limit()
+{
+	# set memory_limit to $php_memory_limit, if no memory_limit is set
+	if grep -q "memory_limit" $nc_base/.user.ini; then
+		logger "- leave $nc_base/.user.ini untouched, memory_limit is already set"
+	else
+		echo "memory_limit=$php_memory_limit" >> $nc_base/.user.ini
+		logger "- $nc_base/.user.ini memory_limit=$php_memory_limit added"
+	fi
+}
+
+function occ_upgrade()
+{
+	# run occ upgrade to make sure, all apps are up to date
+	logger "- make sure everything is up to date before update"
+	logger "- run occ upgrade first"
+	php -d memory_limit=$php_memory_limit $nc_base/occ upgrade
+}
+
+function occ_update_check() {
+	logger "- run occ update:check"
+	if php -d memory_limit=$php_memory_limit $nc_base/occ update:check | grep -q "Everything up to date"; then
+	    logger "- No Updates available"
+		update_available=0
+	else
+		logger "- Updates available"
+		update_available=1
+	fi
+
+	if php -d memory_limit=$php_memory_limit $nc_base/occ update:check | grep "Get more information on how to update"; then
+		logger "- New Nextcloud version is available"
+		update_available=2
+	fi
+}
+
+function occ_app_update()
+{
+	logger "- start updating apps"
+	php -d memory_limit=$php_memory_limit $nc_base/occ app:update --all -n
+}
+
+function update_nc_version()
+{
+	logger "- start update Nextcloud version"
+	php -d memory_limit=$php_memory_limit $nc_base/updater/updater.phar -n
+}
+
+# init check variables
+update_available=0
+
+script_dir=$(dirname "$0")
+# get account directory from username (/www/htdocs/w000000 in the example above)
+# This is especially for all-inkl.com, other providers may need another strategy
+account_base="/www/htdocs/${USER//ssh-}"
 
 # define file with list of installation directories
 # see installations.txt.default
 # set your installation directory under your root account
 # i.e. if your install directory (nextcloud root) is /www/htdocs/w000000/domain.com/nextcloud
 # then add "domain.com/nextcloud" to the installations.txt
-installations="../installations.txt"
+installations="$script_dir/installations.txt"
 
-# get account directory from username (/www/htdocs/w000000 in the example above)
-# This is especially for all-inkl.com, other providers may need another strategy
-account_base="/www/htdocs/${USER//ssh-}"
+# define the php_memory_limit
+php_memory_limit="512M"
 
 while read install_dir; do
 	nc_base=$account_base/$install_dir
 
-	echo -e " "
-	echo -e "\e[33m ==================================\e[0m"
-	echo -e "\e[33m =\e[31m $install_dir \e[0m"
-	echo -e "\e[33m ==================================\e[0m"
-	echo -e "\e[33m - account base: \e[32m$account_base\e[0m"
-	echo -e "\e[33m - nextcloud base dir: \e[32m$nc_base\e[0m"
-	echo -e "\e[33m ==================================\e[0m"
+	logger " "
+	logger "=================================="
+	logger "- nextcloud installation: \e[96m$install_dir"
+	logger "=================================="
+	logger "- account base: \e[32m$account_base"
+	logger "- nextcloud base dir: \e[32m$nc_base"
+	logger "=================================="
 
-	echo -e "\e[33m - chmod 744 $nc_base/occ\e[0m"
+	logger "- chmod 744 $nc_base/occ"
 	chmod 744 $nc_base/occ
 
-	# set memory_limit to $php_memory_limit, if no memory_limit is set
-	if grep -q "memory_limit" $nc_base/.user.ini; then
-		echo -e "\e[33m - leave $nc_base/.user.ini untouched, memory_limit is already set\e[0m"
-	else
-		echo "memory_limit=$php_memory_limit" >> $nc_base/.user.ini
-		echo -e "\e[32m - $nc_base/.user.ini memory_limit=$php_memory_limit added\e[0m"
-	fi
-
-	# occ db:add-missing-indices and :add-missing-columns is called blind
-	echo -e "\e[33m - try db optimizations\e[0m"
-	echo -e "\e[33m - run occ db:add-missing-indices\e[0m"
-	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-indices
-	echo -e "\e[33m - run occ db:add-missing-columns\e[0m"
-	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-columns
-	echo -e "\e[33m - run occ db:add-missing-primary-keys\e[0m"
-	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-primary-keys
-	echo -e "\e[33m - run occ -n db:convert-filecache-bigint\e[0m"
-	php -d memory_limit=$php_memory_limit $nc_base/occ -n db:convert-filecache-bigint
+	set_php_limit
+	occ_upgrade
+	occ_add_indices
+	occ_app_update
+	occ_update_check
 
 	# assuming that occ update:check still reports the same strings
 	# "Everything up to date" means, there are no updates, end script in this case
 	# "update for" means there is an update for at least one app
 	# "Get more information on how to update" means, there is a Nextcloud update available
-	echo -e "\e[33m - run occ update:check\e[0m"
-	php -d memory_limit=$php_memory_limit $nc_base/occ update:check
-	if php -d memory_limit=$php_memory_limit $nc_base/occ update:check | grep -q "Everything up to date"; then
-	    echo -e "\e[32m - No Updates available\e[0m"
-	else
+	if [ "${update_available}" != "0" ] ; then
+		logger "- start updating apps"
+		occ_app_update
 
-		echo -e "\e[33m - start updating apps\e[0m"
-		php -d memory_limit=$php_memory_limit $nc_base/occ app:update --all -n
-
-		if php -d memory_limit=$php_memory_limit $nc_base/occ update:check | grep "Get more information on how to update"; then
-			echo -e "\e[32m - new Nextcloud version is available\e[0m"
-			echo -e "\e[33m - start update Nextcloud version\e[0m"
-			php -d memory_limit=$php_memory_limit $nc_base/updater/updater.phar -n
-
-			echo -e "\e[33m - run occ db:add-missing-indices\e[0m"
-			php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-indices
-
-			echo -e "\e[33m - run occ db:add-missing-columns\e[0m"
-			php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-columns
-
-			# check memory_limit again after Nextcloud update
-			if grep -q "memory_limit" $nc_base/.user.ini; then
-				echo -e "\e[33m - leave $nc_base/.user.ini untouched, memory_limit is already set\e[0m"
-			else
-				echo "memory_limit=$php_memory_limit" >> $nc_base/.user.ini
-				echo -e "\e[32m - $nc_base/.user.ini memory_limit=$php_memory_limit added\e[0m"
-			fi
-
+		if [ "${update_available}" = "2" ] ; then
+			update_nc_version
+			occ_add_indices
+			set_php_limit
 		fi
 	fi
 done <$installations
